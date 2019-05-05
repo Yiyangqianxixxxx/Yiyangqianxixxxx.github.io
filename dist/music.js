6ad4cf6a10a4ae59e0a8dfbb0eeaa3795b766046
“Yiyangqianxixxxx <yh1013024906@outlook.com>
2019-05-05 22:28:25 +0800

    Site updated: 2019-05-05 22:28:21

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: "180度",
+        artist: '孙燕姿',
+        url: 'http://lc-yuaqjuib.cn-n1.lcfile.com/4dd0b2167dfc65cda6a5/%E5%AD%99%E7%87%95%E5%A7%BF%20-%20180%E5%BA%A6.flac',
+        cover: 'http://lc-yuaqjuib.cn-n1.lcfile.com/5e9d57d116a421fac70b/11530262_353800.jpg',
+      }
+    ]
+});
